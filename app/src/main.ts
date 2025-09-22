39aa1fccdb5e9f8a733d77c8290b3b841e2fc393
Thientruong <thanhthien5764@gmail.com>
2025-09-22 06:10:40 +0000

    update: implement swagger

--- app/src/main.ts
+++ app/src/main.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { json, urlencoded } from 'express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { logger: ['error','warn','log'] });
@@ -18,6 +19,18 @@ async function bootstrap() {
   });
   app.use(json({ limit: '5mb' }));
   app.use(urlencoded({ extended: true, limit: '5mb' }));
+
+  const config = new DocumentBuilder()
+    .setTitle('FreeSWITCH Management API')
+    .setDescription('Tài liệu API cho các endpoint quản lý FreeSWITCH')
+    .setVersion('1.0.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  });
 
   const port = process.env.APP_PORT ? parseInt(process.env.APP_PORT, 10) : 3000;
   await app.listen(port);
